196210ab2c46256a7716c63f9994275af9d07280
chef <chef@ChefWs.takara.org>
2015-11-07 21:32:35 +0900

    first commit

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default["mysql"]["root_password"] = 'passw0rd'
+default["mysql"]["db_name"] = 'testdb'
+default["mysql"]["user"]["name"] = 'wordpress'
+default["mysql"]["user"]["password"] = 'wordpress'
+default["mysql"]["node"] = 'service'
+
+
